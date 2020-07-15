0b8f58a74830ac852fb61c8490d2ca74663d4951
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2020-07-14 22:36:10 -0300

    padrao singleton x várias instancias

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -3,7 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CursosService {
 
+  cursos: string[] = ['Angular 2', 'Java', 'Ionic'];
+
+  constructor() {
+    console.log('CursosService');
+  }
+
   getCursos() {
-    return ['Angular 2', 'Java', 'Ionic'];
+    return this.cursos;
+  }
+
+  addCurso(curso: string) {
+    this.cursos.push(curso);
   }
 }
